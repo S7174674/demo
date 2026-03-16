de9fd3785bf0a52e1a34a1b82c8028d8801572ea
S7174674 <nan-nan.hao@fii-foxconn.com>
2026-03-16 14:20:18 +0800

    添加模拟登录功能

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,8 +1,7 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
@@ -11,5 +10,17 @@ public class HelloController {
     @GetMapping("/hi")
     public String hi() {
         return "Hello World!";
+    }
+
+    @Value("${app.login.password}")
+    private String validPassword;
+
+    @PostMapping("/t01")
+    public String login(@RequestParam String username, @RequestParam String password) {
+        if (password.equals(validPassword)) {
+            return "登录成功";
+        } else {
+            return "密码错误, 请联系系统管理员";
+        }
     }
 }
